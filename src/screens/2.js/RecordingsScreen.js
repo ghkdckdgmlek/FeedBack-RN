144b8e81bd334703225d8bcb76d62322662b5d5c
ghkdckdgmlek <131952372+ghkdckdgmlek@users.noreply.github.com>
2024-11-04 19:49:04 +0900

    Update RecordingsScreen.js

--- src/screens/2.js/RecordingsScreen.js
+++ src/screens/2.js/RecordingsScreen.js
@@ -27,7 +27,7 @@ export default function RecordingsScreen({ navigation }) {
     console.log("Token for fetching recordings:", token);
     try {
       const response = await axios.get(
-        "http://192.168.219.148:5001/recordings",
+        "${Your_IP}/recordings",
         {
           headers: { Authorization: `Bearer ${token}` },
         }
@@ -62,7 +62,7 @@ export default function RecordingsScreen({ navigation }) {
           }
           console.log("Token for deleting recording:", token);
           try {
-            await axios.delete(`http://192.168.219.148:5001/recordings/${id}`, {
+            await axios.delete(`${Your_IP}/recordings/${id}`, {
               headers: { Authorization: `Bearer ${token}` },
             });
             // 녹음 목록 업데이트

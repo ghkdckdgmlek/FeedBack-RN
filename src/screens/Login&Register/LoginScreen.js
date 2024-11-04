5d1f17812ea930e906b405fce66b7990c8216a22
ghkdckdgmlek <131952372+ghkdckdgmlek@users.noreply.github.com>
2024-11-04 19:51:26 +0900

    Update LoginScreen.js

--- src/screens/Login&Register/LoginScreen.js
+++ src/screens/Login&Register/LoginScreen.js
@@ -13,7 +13,7 @@ function LoginScreen({ navigation }) {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://192.168.219.148:5001/login", {
+      const response = await axios.post("${Your_IP}:5001/login", {
         name: name,
         password: password,
       });
